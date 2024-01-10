9a170009d6e1f8c7249d186a347d7181b02653fb
LIUyulin <127386844+xueshulitter@users.noreply.github.com>
2024-01-10 16:05:49 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -25,15 +25,15 @@ const USER_CONFIG = {
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'ZcOXlO2CvpZei0D3JqK9pTjBw8zZiwcQei0VGGpXnSM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '11-03',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '宝贝', year: '2002', date: '09-29',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '李四', year: '2002', date: '11-03',
         },
         {
           type: '节日', name: '相识纪念日', year: '2024', date: '01-01',
@@ -42,9 +42,9 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'love_day', date: '2024-01-01' },
+        // 相识纪念日
+        { keyword: 'meet_day', date: '2023-12-29' },
       ],
     },
   ],
