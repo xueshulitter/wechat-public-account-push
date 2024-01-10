6af21cd6ee00b7ce5e3098abaaa040ed99eb5952
LIUyulin <127386844+xueshulitter@users.noreply.github.com>
2024-01-10 16:35:17 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oT50X6Zy7Y81l0DaoMlQ9AMg-mFU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'VstnsbpdqnvH5l0TVVe1cCsVOnoV6PTrjPz1GhvGli8',
+      useTemplateId: '	05dvxDytszuJv54TrNQJMEUFyhBL91sFIK7FKsQPKdY',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-03',
       festivals: [
